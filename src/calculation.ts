3c974a4510510637ff8dc7ab6b12c15f6e568e05
abhikatta <abhinaykatta97@gmail.com>
2025-05-08 13:44:03 +0530

    add comments to not forget again what what does

--- src/calculation.ts
+++ src/calculation.ts
@@ -1,43 +1,47 @@
 interface eventDataProps {
-    rows: number;
-    cols: number;
-    grid: number[][];
-    newGrid: number[][];
-    hue: number;
+  rows: number;
+  cols: number;
+  grid: number[][];
+  newGrid: number[][];
+  hue: number;
 }
 
 self.onmessage = function (event: MessageEvent<eventDataProps>) {
-    let { rows, cols, grid, newGrid, hue } = event.data;
-    for (let i = 0; i < rows; i++) {
-        hue += 1;
-        for (let j = 0; j < cols; j++) {
-            if (grid[i][j] !== 0) {
-                if (i < rows - 1 && j < cols) {
-                    if (grid[i + 1][j] === 0) {
-                        newGrid[i + 1][j] = hue;
-                        newGrid[i][j] = 0;
-                    } else if (
-                        i + 1 < rows &&
-                        j + 1 < cols &&
-                        grid[i + 1][j + 1] === 0
-                    ) {
-                        newGrid[i + 1][j + 1] = hue;
-                        newGrid[i][j] = 0;
-                    } else if (
-                        i + 1 < rows &&
-                        j - 1 >= 0 &&
-                        grid[i + 1][j - 1] === 0
-                    ) {
-                        newGrid[i + 1][j - 1] = hue;
-                        newGrid[i][j] = 0;
-                    } else {
-                        newGrid[i][j] = hue;
-                    }
-                } else {
-                    newGrid[i][j] = hue;
-                }
-            }
+  let { rows, cols, grid, newGrid, hue } = event.data;
+  for (let i = 0; i < rows; i++) {
+    hue += 1;
+    for (let j = 0; j < cols; j++) {
+      // 0 = unfilled cell, 1(or hue value) means filled cell
+      // so this is, if the hovered cell is filled
+      if (grid[i][j] !== 0) {
+        // if the hovered cell is atleast the last column and the cell has a row below it
+        if (i < rows - 1 && j < cols) {
+          // if the cell below hovered cell is 0(not filled)
+          if (grid[i + 1][j] === 0) {
+            newGrid[i + 1][j] = hue; //fill the cell below hovered cell in newgrid (newgrid because repaint)
+            newGrid[i][j] = 0; //unfill the cell hovered cell in newgrid
+
+            // So here, the sand first looks to right bottom cell, so it always falls to right
+            // if its right bottom cell is empty
+
+            // slide the sand to the right bottom cell if right bottom cell exits and its empty
+          } else if (i + 1 < rows && j + 1 < cols && grid[i + 1][j + 1] === 0) {
+            newGrid[i + 1][j + 1] = hue;
+            newGrid[i][j] = 0;
+            // else slide the sand to the left bottom cell if left bottom cell exits and its empty
+          } else if (i + 1 < rows && j - 1 >= 0 && grid[i + 1][j - 1] === 0) {
+            newGrid[i + 1][j - 1] = hue;
+            newGrid[i][j] = 0;
+          } else {
+            newGrid[i][j] = hue;
+          }
         }
+        // fill the current cell if all below cells are filled, i think
+        else {
+          newGrid[i][j] = hue;
+        }
+      }
     }
-    self.postMessage(newGrid);
+  }
+  self.postMessage(newGrid);
 };
--- src/main.ts
+++ src/main.ts
@@ -12,101 +12,88 @@ const app = document.getElementById("app");
 const canvas: HTMLCanvasElement = document.createElement("canvas");
 
 function make2DArray(rows: number, cols: number): number[][] {
-    let arr = new Array(rows);
-    for (let i = 0; i < rows; i++) {
-        arr[i] = new Array(cols).fill(0);
-    }
-    return arr;
+  let arr = new Array(rows);
+  for (let i = 0; i < rows; i++) {
+    arr[i] = new Array(cols).fill(0);
+  }
+  return arr;
 }
 
 grid = make2DArray(rows, cols);
 const newGrid = make2DArray(rows, cols);
 
 const setupGrid = () => {
-    canvas.width = rows * 10;
-    canvas.height = cols * 10;
-    if (canvas.getContext) {
-        const ctx = canvas.getContext("2d")!;
-        const cellWidth = canvas.width / cols;
-        const cellHeight = canvas.height / rows;
-
-        for (let i = 0; i < rows; i++) {
-            for (let j = 0; j < cols; j++) {
-                ctx.roundRect(
-                    j * cellWidth,
-                    i * cellHeight,
-                    cellWidth,
-                    cellHeight
-                );
-                ctx.fillStyle =
-                    grid[i][j] === 0
-                        ? "black"
-                        : `hsl(${(grid[i][j] + 1) % 360}, 100%, 40%)`; // hot sand ig
-                ctx.fillRect(
-                    j * cellWidth,
-                    i * cellHeight,
-                    cellWidth,
-                    cellHeight
-                );
-            }
-        }
+  canvas.width = rows * 10;
+  canvas.height = cols * 10;
+  if (canvas.getContext) {
+    const ctx = canvas.getContext("2d")!;
+    const cellWidth = canvas.width / cols;
+    const cellHeight = canvas.height / rows;
+    // dividing the grid on canvas and drawing the squares(cells) and filling them hue or black
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        ctx.roundRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+        ctx.fillStyle =
+          grid[i][j] === 0
+            ? "black"
+            : `hsl(${(grid[i][j] + 1) % 360}, 100%, 40%)`; // hot sand ig
+        ctx.fillRect(j * cellWidth, i * cellHeight, cellWidth, cellHeight);
+      }
     }
+  }
 };
 
 setupGrid();
 
 canvas.addEventListener("mousemove", (e) => {
-    const canvasBounds = canvas.getBoundingClientRect();
-    const canvasX = canvasBounds.x;
-    const canvasY = canvasBounds.y;
+  const canvasBounds = canvas.getBoundingClientRect();
+  const canvasX = canvasBounds.x;
+  const canvasY = canvasBounds.y;
 
-    const mouseX = e.clientX - canvasX;
-    const mouseY = e.clientY - canvasY;
+  const mouseX = e.clientX - canvasX;
+  const mouseY = e.clientY - canvasY;
+  // calc coordinates of the cell where the mouse is hovered on the canvas
+  const x = Math.floor((mouseX * initialValue) / canvas.width);
+  const y = Math.floor((mouseY * initialValue) / canvas.height);
+  //   if the cell is well inside the canvas boundaries, work on filling the cell
+  if (x >= 0 && x <= initialValue - 1 && y >= 0 && y <= initialValue - 1) {
+    grid[y][x] = 1;
+    setupGrid();
+  }
+});
+
+canvas.addEventListener("touchmove", (e) => {
+  const canvasBounds = canvas.getBoundingClientRect();
+  const canvasX = canvasBounds.x;
+  const canvasY = canvasBounds.y;
+  const touches = e.touches;
+
+  for (let i = 0; i < touches.length; i++) {
+    const mouseX = touches[i].clientX - canvasX;
+    const mouseY = touches[i].clientY - canvasY;
 
     const x = Math.floor((mouseX * initialValue) / canvas.width);
     const y = Math.floor((mouseY * initialValue) / canvas.height);
     if (x >= 0 && x <= initialValue - 1 && y >= 0 && y <= initialValue - 1) {
-        grid[y][x] = 1;
-        setupGrid();
+      grid[y][x] = 1;
+      setupGrid();
     }
-});
-
-canvas.addEventListener("touchmove", (e) => {
-    const canvasBounds = canvas.getBoundingClientRect();
-    const canvasX = canvasBounds.x;
-    const canvasY = canvasBounds.y;
-    const touches = e.touches;
-
-    for (let i = 0; i < touches.length; i++) {
-        const mouseX = touches[i].clientX - canvasX;
-        const mouseY = touches[i].clientY - canvasY;
-
-        const x = Math.floor((mouseX * initialValue) / canvas.width);
-        const y = Math.floor((mouseY * initialValue) / canvas.height);
-        if (
-            x >= 0 &&
-            x <= initialValue - 1 &&
-            y >= 0 &&
-            y <= initialValue - 1
-        ) {
-            grid[y][x] = 1;
-            setupGrid();
-        }
-    }
+  }
 });
 
 app?.append(canvas);
 
 const updateGrid = () => {
-    calculator.postMessage({ rows, cols, grid, newGrid, hue });
-    calculator.onmessage = function (e) {
-        grid = e.data;
-        setupGrid();
-    };
-    calculator.onerror = function (e) {
-        console.error(e.message);
-    };
-    requestAnimationFrame(updateGrid);
+  calculator.postMessage({ rows, cols, grid, newGrid, hue });
+  calculator.onmessage = function (e) {
+    grid = e.data;
+    setupGrid();
+  };
+  calculator.onerror = function (e) {
+    console.error(e.message);
+  };
+  requestAnimationFrame(updateGrid);
 };
 
+// start animation
 requestAnimationFrame(updateGrid);
