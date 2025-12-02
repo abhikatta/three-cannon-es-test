a4e9878ab75cadc4a021c58b41c9161b14ee8977
abhikatta <abhinaykatta97@gmail.com>
2025-12-02 16:41:25 +0530

    adds comments for future ref

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import {
   GridHelper,
   Mesh,
   MeshStandardMaterial,
+  PerspectiveCamera,
   PointLight,
   PointLightHelper,
+  Scene,
   TorusGeometry,
+  WebGLRenderer,
 } from "three";
-import { PerspectiveCamera, Scene, WebGLRenderer } from "three";
 import { OrbitControls } from "three/examples/jsm/Addons.js";
-import { saveSceneState } from "three/src/renderers/common/RendererUtils.js";
 
+// 1. setup
 const scene = new Scene();
 const camera = new PerspectiveCamera(
   75,
@@ -18,42 +20,40 @@ const camera = new PerspectiveCamera(
   0.1,
   1000
 );
-
-camera.position.set(0, 0, 30);
+camera.position.set(0, 0, 30); // zoomed outward on z so its 30 points away on z-axis from (0,0,0) point
 const element = document.getElementById("bg");
-
 const renderer = new WebGLRenderer({
   canvas: element!,
 });
-
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 
+// 2. adding a random object
 const geometry = new TorusGeometry(10, 3, 100, 100);
 const material = new MeshStandardMaterial({
   color: 0xff6347,
 });
-
 const torus = new Mesh(geometry, material);
-
 scene.add(torus);
 
-const pointLight = new PointLight(0xffffff);
+// 3. adding some light
+const pointLight = new PointLight(0xffffff); // a flashlight typa light, but luminates in all directions ig
 pointLight.intensity = 1000;
+const ambientLight = new AmbientLight(0xffffff); // adding light on everything, like its daytime
+scene.add(pointLight, ambientLight);
 
-const ambientLight = new AmbientLight(0xffffff);
-
-const pointLightHelper = new PointLightHelper(pointLight);
+const pointLightHelper = new PointLightHelper(pointLight); // a helper that shows where the pointLight is
+pointLight.position.set(15, 2, 0);
 
 scene.add(pointLightHelper);
 
-scene.add(pointLight, ambientLight);
-pointLight.position.set(15, 2, 0);
-
+// 4. adding orbital controls and grid helper for adding camera movement and understanding movements
+const orbitalControls = new OrbitControls(camera, renderer.domElement); // not required for this to be added to scene as its controls not an object
 const gridHelper = new GridHelper(200, 50);
-const orbitalControls = new OrbitControls(camera, renderer.domElement);
 
 scene.add(gridHelper);
+
+// 5. animation loop
 function animate() {
   renderer.render(scene, camera);
 
