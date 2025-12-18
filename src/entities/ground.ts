f7e1324d443075285b6a95e60350d2f658ee9664
abhikatta <abhinaykatta97@gmail.com>
2025-12-18 13:34:14 +0530

    AK | initialize single physics ground instead of multiple

--- src/entities/ground.ts
+++ src/entities/ground.ts
@@ -3,38 +3,29 @@ import { Box3, Object3D, Scene, Vector3 } from "three";
 
 export default class Ground {
   constructor(ground3dModel: Object3D, scene: Scene, world: World) {
+    const tilesX = 10;
+    const tilesZ = 10;
     const box = new Box3().setFromObject(ground3dModel);
     const size = new Vector3();
     box.getSize(size);
 
-    const shape = new Vec3(size.x / 2, size.y / 2, size.z / 2);
-
-    // const groundBody = new Body({
-    //   shape: new Box(shape),
-    //   type: Body.STATIC,
-    //   material: new Material(),
-    // });
-
-    const tilesX = 10;
-    const tilesZ = 10;
+    const groundBody = new Body({
+      shape: new Box(
+        new Vec3((size.x / 2) * tilesX, size.y / 2, (size.z / 2) * tilesZ)
+      ),
+      type: Body.STATIC,
+      material: new Material(),
+    });
+    groundBody.position.set(0, 0, 0);
+    world.addBody(groundBody);
 
     for (let x = -tilesX / 2; x < tilesX / 2; x++) {
       for (let z = -tilesZ / 2; z < tilesZ / 2; z++) {
         const tile = ground3dModel.clone(true);
 
-        const groundBody = new Body({
-          shape: new Box(shape),
-          type: Body.STATIC,
-          material: new Material(),
-        });
-
-        groundBody.position.set(x * size.x, 0, z * size.z);
-
-        tile.position.copy(groundBody.position);
-        tile.quaternion.copy(groundBody.quaternion);
+        tile.position.set(x * size.x, 0, z * size.z);
 
         scene.add(tile);
-        world.addBody(groundBody);
       }
     }
   }
